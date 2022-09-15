df1da563cfb5be447a8b27922cd5d06311447e0b
Earvin Macias <isaacmacias2209@gmail.com>
2022-09-15 09:43:09 -0500

    Desarrollo de capa Acceso a Datos

---
+++ AccesoDatosTienda/AccesoDatosProductos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConectarBd;
+using EntidadesTienda;
+namespace AccesoDatosTienda
+{
+    public class AccesoDatosProductos
+    {
+        Base b = new Base("localhost", "root", "", "tienda2");
+        public void Borrar(Producto producto)
+        {
+            b.Comando(string.Format("call p_deleteProducto({0})", producto.Id));
+        }
+
+        public void Guardar(Producto producto)
+        {
+            b.Comando(string.Format("call p_insertOrUpdateProducto('{0}', '{1}', {2}, {3})",producto.Nombre,
+                producto.Descripcion, producto.Precio, producto.Id));
+        }
+        public DataSet Mostrar(string filtro)
+        {
+            return b.Obtener(string.Format("call p_showProductos('%{0}%')", filtro), "productos");
+        }
+    }
+}
---
+++ EntidadesTienda/Producto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadesTienda
+{
+    public class Producto
+    {
+        public Producto(int id, string nombre, string descripcion, double precio)
+        {
+            Id = id;
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Precio = precio;
+        }
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public double Precio { get; set; }
+    }
+}
