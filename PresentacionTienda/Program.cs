cbc8f8eb3ea9ef155d45498afda5ac47ea8d6822
Earvin Macias <isaacmacias2209@gmail.com>
2022-09-17 15:16:25 -0500

    Proyecto finalizado

---
+++ PresentacionTienda/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+namespace PresentacionTienda
+{
+    public class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.Run(new FrmProductos());
+        }
+    }
+}
